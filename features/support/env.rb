21364df07a68ed3ee35a21b5252d017330beadd7
中田 宏昭 <pascalm3@chuuden-no-MacBook-Air.local>
2011-09-11 18:12:17 +0900

    apply capybara-webkit

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,19 +1,28 @@
 # RSpec
-require 'spec/expectations'
+require 'rspec/expectations'
 require 'cucumber/formatter/unicode'
 
 
 # Capybara
-require 'capybara'
-require 'capybara/dsl'
+require 'bundler/setup'
 require 'capybara/cucumber'
-require 'capybara/mechanize/cucumber'
+require 'capybara-webkit'
 
-Capybara.default_driver = :mechanize
-Capybara.javascript_driver = :mechanize
+Capybara.default_driver = :webkit
+Capybara.javascript_driver = :webkit
 Capybara.app_host = 'http://www.google.co.jp'
 Capybara.run_server = false
 
 
+if %w(yes y on).include?(ENV['HEADLESS'])
+  require 'headless'
+ 
+  headless = Headless.new
+  headless.start
+ 
+  at_exit do
+    headless.destroy
+  end
+end
 
 World(Capybara)
